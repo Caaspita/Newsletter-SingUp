d19748790a20c5f7707e0805f816bd94f484e3a6
Caspita <lucascaspani@gmail.com>
2024-02-04 21:30:50 -0300

    Falta solo los mq

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+    // Evitar el reinicio de la página
+    event.preventDefault();
 
-
+});
 function validacion(){
-    var expreg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    var correo = document.getElementById('correo')
+    var email = document.getElementById('correo');
+    var expemail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const error = document.getElementById('error');
+    const page1 = document.getElementById('content');
+    const page2 = document.getElementById('success-content');
 
-    if(expreg.test(correo.value)){
+    if(expemail.test(email.value)){
         console.log('valido')
+        page1.classList.add('ocultar')
+        page2.classList.remove('ocultar');
+    
     }else{
-        console.log('invalido')
+        console.log('falso')
+        error.classList.remove('ocultar');
+        email.classList.add('error-input');
     }
-
 }
